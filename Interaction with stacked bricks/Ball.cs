49eccb497512ad57f80bed32c532b6f1650e818b
Moldovan Daniela-Mihaela <57368697+mikellah@users.noreply.github.com>
2020-05-02 16:48:54 +0300

    Add files via upload
    Using a sphere object to interact with stacked bricks.

---
+++ Interaction with stacked bricks/Ball.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public float speed = 500f;
+    public Vector3 jump;
+    private bool isGrounded;
+    private Rigidbody body;
+    public float jumpPower = 1f;
+   
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //for movement input:
+        float inputX = Input.GetAxis("Horizontal");
+        float inputZ = Input.GetAxis("Vertical");
+        float moveX = inputX * speed * Time.deltaTime;
+        float moveZ = inputZ * speed * Time.deltaTime;
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
+        body.AddForce(moveX,0,moveZ);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+
+            body.AddForce(jump * jumpPower, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+    void OnCollisionStay()
+    {
+        isGrounded = true;
+    }
+    void OnCollisionExit()
+    {
+        isGrounded = false;
+    }
+}
---
+++ Interaction with stacked bricks/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Vector3 offset;
+    public Transform target;
+    public float smoothSpeed = 10f;
+
+    // Start is called before the first frame update
+    private void FixedUpdate()
+    {
+         Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothPosition = Vector3.Lerp(transform.position,desiredPosition,smoothSpeed*Time.deltaTime);
+
+        transform.position = smoothPosition;
+        transform.LookAt(target);
+    }
+}
